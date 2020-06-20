7d1a387f841e9bac7298ac4ad54baebe4d633bb8
Somnoboy <66186271+Somnoboy@users.noreply.github.com>
2020-06-20 00:32:29 -0400

    Add project files.

---
+++ TestASP2/Default.aspx.vb
@@ -0,0 +1,11 @@
+﻿Public Class _Default
+    Inherits Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+
+    End Sub
+
+    Private Sub btnTry_Click(sender As Object, e As EventArgs) Handles btnTry.Click
+        lblTry.Text = "I tried, and it worked."
+    End Sub
+End Class
---
+++ TestASP2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="TestASP2.Global_asax" Language="vb" %>
